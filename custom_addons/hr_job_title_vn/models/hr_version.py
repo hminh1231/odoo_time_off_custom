22231052a082b46c7f9334d5913fe5b2e8d3b2bd
Song Khoi <songkhoi123@gmail.com>
2026-04-23 16:00:40 +0700

    add: job title module

---
+++ custom_addons/hr_job_title_vn/models/hr_version.py
@@ -0,0 +1,31 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+# Static list: Odoo passes the model as first arg to callable selection helpers; use a list, not a 0-arg function.
+JOB_TITLE_SELECTION = [
+    ("nhân viên", "nhân viên"),
+    ("trưởng nhóm", "trưởng nhóm"),
+    ("trưởng BP", "trưởng BP"),
+    ("kiểm soát", "kiểm soát"),
+    ("trưởng phòng HCNS", "trưởng phòng HCNS"),
+    ("giám đốc", "giám đốc"),
+]
+
+
+class HrVersion(models.Model):
+    _inherit = "hr.version"
+
+    job_title = fields.Selection(
+        selection=JOB_TITLE_SELECTION,
+        string="Job Title",
+        tracking=True,
+    )
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "job_title" in vals:
+            for ver in self:
+                if ver.employee_id:
+                    ver.employee_id._check_manager_job_title_hierarchy()
+        return res
