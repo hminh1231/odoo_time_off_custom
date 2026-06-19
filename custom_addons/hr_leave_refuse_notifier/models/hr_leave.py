1c4f0c8011487bdf138ffedbf32aae9f3628761e
Song Khoi <songkhoi123@gmail.com>
2026-06-19 10:08:12 +0700

    add refuse

--- custom_addons/hr_leave_refuse_notifier/models/hr_leave.py
+++ custom_addons/hr_leave_refuse_notifier/models/hr_leave.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from odoo import _, models
+
+_logger = logging.getLogger(__name__)
 
 
 class HrLeave(models.Model):
@@ -7,14 +11,29 @@ class HrLeave(models.Model):
 
     def _notify_manager(self):
         result = super()._notify_manager()
-        self._notify_refuse_ticket_notifier()
+        try:
+            self._notify_refuse_ticket_notifier()
+        except Exception:
+            _logger.exception("hr_leave_refuse_notifier: failed to notify refuse ticket notifier")
         return result
 
     def _notify_refuse_ticket_notifier(self):
         for leave in self:
             employee = leave.holiday_status_id.refuse_notify_employee_id
             if not employee or not employee.user_id:
+                _logger.info(
+                    "hr_leave_refuse_notifier: skip leave_id=%s employee=%s has_user=%s",
+                    leave.id,
+                    employee.id if employee else None,
+                    bool(employee.user_id) if employee else False,
+                )
                 continue
+            _logger.info(
+                "hr_leave_refuse_notifier: notifying employee_id=%s (%s) for leave_id=%s",
+                employee.id,
+                employee.user_id.login,
+                leave.id,
+            )
             leave.sudo().message_post(
                 partner_ids=employee.user_id.partner_id.ids,
                 body=_(
