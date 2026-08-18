8087a992de07a53bcaca3dc85ff32726ab37cebb
Song Khoi <songkhoi123@gmail.com>
2026-08-18 09:41:22 +0700

    create store manage field

--- custom_addons/hr_store/models/hr_employee.py
+++ custom_addons/hr_store/models/hr_employee.py
@@ -13,3 +13,12 @@ class HrEmployee(models.Model):
         tracking=True,
         help='Cửa hàng mà nhân viên thuộc về.',
     )
+    managed_store_ids = fields.Many2many(
+        'hr.store',
+        'hr_employee_managed_store_rel',
+        'employee_id',
+        'store_id',
+        string='Cửa hàng quản lí',
+        tracking=True,
+        help='Các cửa hàng mà nhân viên này quản lí.',
+    )
